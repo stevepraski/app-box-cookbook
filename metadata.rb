f4b81673635940150703317cc533062bdd13b4ad
stevepraski <stevenpraski@gmail.com>
2018-02-12 15:54:48 -0800

    fixed repo skeleton

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name 'app-box-cookbook'
+maintainer 'Steven Praski'
+maintainer_email 'stevepraski@users.noreply.github.com'
+license 'Apache-2.0'
+source_url 'https://github.com/stevepraski/app-box-cookbook'
+issues_url 'https://github.com/stevepraski/app-box-cookbook/issues'
+description 'Installs/Configures app-box-cookbook'
+long_description 'Installs/Configures app-box-cookbook'
+chef_version '>= >= 13.2.20' if respond_to?(:chef_version)
+supports 'centos', '= 7.3'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: app-box-cookbook
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
